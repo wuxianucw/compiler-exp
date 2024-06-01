fa5ef5e9ffcdc9f62b6a48e2970dab1f66f190b7
无限UCW <x-wuxian@qq.com>
2024-06-01 23:33:43 +0800

    lexer: add error position & unit test

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,15 +1,21 @@
 pub mod token;
 
-use std::io::{Bytes, Read, Error as IOError};
+use std::io::{BufRead, Bytes, Error as IOError};
 
 use thiserror::Error;
 
 pub use token::Token;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Position {
+    pub line: u32,
+    pub column: u32,
+}
+
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("invalid byte `{0}`")]
-    InvalidByte(u8),
+    #[error("invalid byte `{0}` at line {}, column {}", .1.line, .1.column)]
+    InvalidByte(u8, Position),
 
     #[error("IO error occurred")]
     IOError(#[from] IOError),
@@ -21,19 +27,33 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub struct Lexer<R> {
     bytes: Bytes<R>,
     current: Option<u8>,
+    position: Position,
 }
 
-impl<R: Read> Lexer<R> {
+impl Position {
+    pub fn new(line: u32, column: u32) -> Self {
+        Self { line, column }
+    }
+}
+
+impl<R: BufRead> Lexer<R> {
     pub fn new(reader: R) -> Result<Self> {
         let mut bytes = reader.bytes();
         let current = bytes.next().transpose().map_err(Error::IOError)?;
         Ok(Self {
             bytes,
             current,
+            position: Position::new(1, 0),
         })
     }
 
     fn advance(&mut self) -> Result<()> {
+        if matches!(self.current, Some(b'\n')) {
+            self.position.line += 1;
+            self.position.column = 0;
+        } else {
+            self.position.column += 1;
+        }
         self.current = self.bytes.next().transpose().map_err(Error::IOError)?;
         Ok(())
     }
@@ -52,6 +72,7 @@ impl<R: Read> Lexer<R> {
         }
 
         let current = self.current.unwrap();
+        let current_pos = self.position;
         self.advance()?;
         match current {
             b'+' => Ok(Some(Token::Plus)),
@@ -81,9 +102,9 @@ impl<R: Read> Lexer<R> {
                 }
                 Ok(Some(Token::Number(num)))
             }
-            x @ (b'A'..=b'Z' | b'a'..=b'z') => {
+            x @ (b'A'..=b'Z' | b'a'..=b'z' | b'_') => {
                 let mut buf = vec![x];
-                while self.current.is_some_and(|x| x.is_ascii_alphabetic()) {
+                while self.current.is_some_and(|x| x.is_ascii_alphanumeric() || x == b'_') {
                     buf.push(self.current.unwrap());
                     self.advance()?;
                 }
@@ -102,15 +123,72 @@ impl<R: Read> Lexer<R> {
                     _ => Token::Id(word)
                 }))
             }
-            x => Err(Error::InvalidByte(x))
+            x => Err(Error::InvalidByte(x, current_pos))
         }
     }
 }
 
-impl<R: Read> Iterator for Lexer<R> {
+impl<R: BufRead> Iterator for Lexer<R> {
     type Item = Result<Token>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next_token().transpose()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tokenize() {
+        let input = "  \n\tprogram;:=_aaa1_b0 vAr:VAR 1234567890 Var6".as_bytes();
+        let mut lexer = Lexer::new(input).unwrap();
+
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Program));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Semicolon));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Assign));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Id("_aaa1_b0".into())));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Id("vAr".into())));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Colon));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Var));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Number(1234567890)));
+        assert_eq!(lexer.next_token().unwrap(), Some(Token::Id("Var6".into())));
+        assert_eq!(lexer.next_token().unwrap(), None);
+    }
+
+    #[test]
+    fn non_ascii_is_invalid() {
+        let input = "你好，世界。".as_bytes();
+        let invalid_byte = input[0];
+
+        let mut lexer = Lexer::new(input).unwrap();
+        let result = lexer.next().unwrap();
+
+        match result.unwrap_err() {
+            Error::InvalidByte(byte, position) => {
+                assert_eq!(byte, invalid_byte);
+                assert_eq!(position, Position::new(1, 0));
+            }
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn invalid_byte_position() {
+        let input = "program\nprog\x01ram".as_bytes();
+        let invalid_byte = b'\x01';
+        let invalid_pos = Position::new(2, 4);
+
+        let mut lexer = Lexer::new(input).unwrap();
+        let result = lexer.nth(2).unwrap();
+
+        match result.unwrap_err() {
+            Error::InvalidByte(byte, position) => {
+                assert_eq!(byte, invalid_byte);
+                assert_eq!(position, invalid_pos);
+            }
+            _ => panic!(),
+        }
     }
 }
--- lexer/src/token.rs
+++ lexer/src/token.rs
@@ -1,6 +1,6 @@
 use std::fmt::{Display, Formatter, Result as FmtResult, Write};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
     // Reserved words
     Program,
